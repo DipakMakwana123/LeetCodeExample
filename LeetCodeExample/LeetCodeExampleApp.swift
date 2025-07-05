fd4651fff212cf01dce60098245d17320826e40e
DipakMakwana123 <dipakmakwana123@users.noreply.github.com>
2025-07-05 13:42:59 +0530

    Initial Commit

---
+++ LeetCodeExample/LeetCodeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeExampleApp.swift
+//  LeetCodeExample
+//
+//  Created by Dipak Makwana on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
